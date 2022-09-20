325db12a8096f7eebf138dc865d4fb674d8748bf
Jordan Farelli <jordanfarelli@gmail.com>
2022-09-20 14:48:01 -0600

    Creating initial comoponent files

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import Nav from '../Nav/Nav';
+
+import './App.css';
+
+const App = ( ) => {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import './Nav.css';
+import wilson from './src/'
+import React from 'react';
+
+const Nav = ( ) => {
+  return (
+    <div>
+      {wilson}
+    </div>
+  )
+}
+
+export default Nav;
